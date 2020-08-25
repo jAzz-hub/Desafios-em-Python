9828a9dc13ed6197f7314814a8b2e18d5d01b769
João Gustavo Silva Guimarães <48455299+jAzz-hub@users.noreply.github.com>
2020-08-25 10:21:36 -0300

    Update Definindo maior e menor número através de lista.py

--- Definindo maior e menor número através de lista.py
+++ Definindo maior e menor número através de lista.py
@@ -1,6 +1,7 @@
 #Usando While para definir maior e menor valores de uma lista:
 
 c=0
+
 l=[]
 
 while c<3:
@@ -8,6 +9,9 @@ while c<3:
     n=int(input(f'Digite aqui o {c}º valor: '))
     l.append(n)
 
+    
 l.sort()
+
 print(l)
+
 print(f'O maior valor da lista será {l[-1]} e o menor deles será {l[0]}')
