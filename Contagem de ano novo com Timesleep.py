381f157f7cf157609c3b73ab704a31d1f4a6ff16
João Gustavo Silva Guimarães <48455299+jAzz-hub@users.noreply.github.com>
2020-08-27 15:25:57 -0300

    Add files via upload

---
+++ Contagem de ano novo com Timesleep.py
@@ -0,0 +1,14 @@
+from time import sleep
+
+for c in range(10,-1,-1):
+    sleep(1)
+    print(c)
+    
+sleep(0.7)
+print(11*'=-')
+print('FOGOS NO CÉU!!')    
+print(11*'=-')
+sleep(1)
+print(11*'=-')
+print('''FELIZ ANO NOVO!!''')
+print(11*'=-')
