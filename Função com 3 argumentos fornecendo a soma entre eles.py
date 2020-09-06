52be4792ca913106afdf373e0b4106ad24eeadfa
João Gustavo Silva Guimarães <48455299+jAzz-hub@users.noreply.github.com>
2020-09-06 17:44:48 -0300

    Add files via upload

---
+++ Função com 3 argumentos fornecendo a soma entre eles.py
@@ -0,0 +1,17 @@
+#Fornecendo valores para serem somados dentro de uma função:
+
+
+n1=int(input('Insira aqui o 1 fator: '))
+
+n2=int(input('Insira aqui o 2 fator: '))
+
+n3=int(input('Insira aqui o 3 fator: '))
+
+
+def soma(x,y,z):
+    s=x+y+z
+    print(s)
+
+
+soma(n1,n2,n3)
+
