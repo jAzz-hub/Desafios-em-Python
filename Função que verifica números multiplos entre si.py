2fc9b74cc367e0e97ce6e8aca6833f167a5b63fb
João Gustavo Silva Guimarães <48455299+jAzz-hub@users.noreply.github.com>
2020-09-07 17:48:58 -0300

    Add files via upload

---
+++ Função que verifica números multiplos entre si.py
@@ -0,0 +1,27 @@
+#Verificando se os argumentos de uma função representam números múltiplos entre si:
+
+n1=int(input('Insira algum número número:  '))
+
+n2=int(input('Insira outro número número: '))
+
+
+print('Verificando se ambos são multiplos entre si')
+
+
+def is_multiple(a,b):
+    if a%b==0:
+        print(f'{a} e {b} São multiplos entre si!')
+        return(True)
+
+    elif b%a==0:
+        print(f'{a} e {b} São multiplos entre si!')
+        return(True)
+
+    else:
+        print(f'{a} e {b} Não são multiplos entre si!')
+        return(False)
+
+
+is_multiple(n1,n2):
+
+
