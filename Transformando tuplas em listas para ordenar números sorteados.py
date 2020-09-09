db9c08cfdbfda284a47d992b4f0b6645d2a5319f
João Gustavo Silva Guimarães <48455299+jAzz-hub@users.noreply.github.com>
2020-09-09 17:10:50 -0300

    Add files via upload

---
+++ Transformando tuplas em listas para ordenar números sorteados.py
@@ -0,0 +1,25 @@
+#Usando tuplas e listas para definir maiores e menores números sorteados:
+
+from random import randint
+
+c=str(randint(0,9))
+
+
+for i in range(0,4):
+    a=str(randint(0,9))
+    a+=c
+    c=a
+    
+
+d=tuple(a)
+
+print(f'os valores sorteados foram{d}')
+
+l=list(d)
+
+l=sorted(l)
+
+print(f'o maior valor foi {l[4]}')
+
+
+print(f'e o menor valor foi {l[0]}')
